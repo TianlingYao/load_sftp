8083915fce3003679ea6bc26513176c6623e18bd
TianlingYao <164115510+TianlingYao@users.noreply.github.com>
2024-03-20 21:07:54 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='upload_blobcontainer',
+  version='0.0.1',
+  author='Skye.Yao',
+  url=r'https://github.com/TianlingYao/load_sftp',
+  author_email='skye.yao@ap.jll.com',
+  description='upload file into blob container wheel',
+  packages=find_packages(exclude=['test**']),
+  # entry_points={
+  #   'group_1': 'run=my_test_code.__main__:main
+  # },
+  install_requires=[
+# 'pandas',
+# 'datetime',
+# 'json',
+'azure-storage-blob==12.18.3',
+'cryptography',
+# 'hashlib',
+# 'base64',
+# 'os',
+'aliyunsdkcore',
+'alibabacloud-credentials',
+'aliyun-python-sdk-kms==2.16.2'
+  ]
+)
